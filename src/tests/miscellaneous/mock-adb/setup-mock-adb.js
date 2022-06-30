c9b826033509c38af4a200f21eef4a6f7dee7049
Katy DeCorah <katydecorah@microsoft.com>
2022-06-30 10:50:55 -0400

    Experiment: use fs promises

--- src/tests/miscellaneous/mock-adb/setup-mock-adb.js
+++ src/tests/miscellaneous/mock-adb/setup-mock-adb.js
@@ -1,8 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
-const { promisify } = require('util');
 const {
     commonAdbConfigs,
     delayAllCommands,
@@ -19,26 +18,37 @@ const {
 } = require('./common-adb-configs');
 const { fileWithExpectedLoggingPath, fileWithMockAdbConfig } = require('./common-file-names.js');
 
-const exists = promisify(fs.exists);
-const writeFile = promisify(fs.writeFile);
-
 const mockAdbFolder = path.join(__dirname, '../../../../drop/mock-adb');
 
 const binPath = path.join(mockAdbFolder, process.platform === 'win32' ? 'adb.exe' : 'adb');
 const configPath = path.join(mockAdbFolder, fileWithMockAdbConfig);
 
 async function setupMockAdb(config, logFolderName, ...extraLogNames) {
-    if (!(await exists(binPath))) {
+    let filehandle;
+    try {
+        filehandle = await fs.open(binPath);
+    } catch (err) {
         throw new Error(
             `Could not find mock-adb executable at expected path "${binPath}", try rebuilding with yarn build:mock-adb`,
         );
+    } finally {
+        await filehandle?.close();
     }
 
-    await writeFile(
-        path.join(mockAdbFolder, fileWithExpectedLoggingPath),
-        path.join(logFolderName, ...extraLogNames),
-    );
-    await writeFile(configPath, JSON.stringify(config, null, 2 /*spaces per indent*/));
+    try {
+        await fs.writeFile(
+            path.join(mockAdbFolder, fileWithExpectedLoggingPath),
+            path.join(logFolderName, ...extraLogNames),
+        );
+    } catch (err) {
+        console.error(err);
+    }
+
+    try {
+        await fs.writeFile(configPath, JSON.stringify(config, null, 2));
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 module.exports = {
